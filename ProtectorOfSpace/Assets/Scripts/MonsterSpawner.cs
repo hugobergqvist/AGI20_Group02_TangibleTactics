d27c6d0970c76f979dbaeb792663a7c5eac0b441
hugobergqvist <hugo@bergqvist.com>
2020-10-22 15:45:33 +0200

    added wavespawner and UItext

--- ProtectorOfSpace/Assets/Scripts/MonsterSpawner.cs
+++ ProtectorOfSpace/Assets/Scripts/MonsterSpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MonsterSpawner : MonoBehaviour
 {
@@ -15,6 +16,16 @@ public class MonsterSpawner : MonoBehaviour
     private List<GameObject> unityGameObjects = new List<GameObject>();
     public GameObject Monster;
 
+    public float timeBetweenWaves = 20f;
+    private float countDown = 2f;
+
+    public Text waveCountdownText;
+
+    public Text enemyCountText;
+
+    private int waveIndex = 0;
+
+    private int monsterCount = 0;
 
     private AudioSource audioSource;
 
@@ -28,55 +39,62 @@ public class MonsterSpawner : MonoBehaviour
         instance = this;
     }
 
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-        SetRanges();
-    }
-    void Update()
-    {
-        _xAxis = UnityEngine.Random.Range(Min.x, Max.x) * (Random.Range(0, 2) * 2 - 1);
-        _yAxis = 5f;
-        _zAxis = UnityEngine.Random.Range(Min.z, Max.z) * (Random.Range(0, 2) * 2 - 1);
-        _randomPosition = new Vector3(_xAxis, _yAxis, _zAxis);
-
-        if (unityGameObjects.Count < 20)
-        {
-            InstantiateRandomObjects();
-        }
-    }
     //Here put the ranges where your object will appear, or put it in the inspector.
     private void SetRanges()
     {
         Min = new Vector3(15, 0, 15); //Random value.
         Max = new Vector3(45, 0, 45); //Another ramdon value, just for the example.
     }
-    private void InstantiateRandomObjects()
-    {
-        if (_canInstantiate)
-        {
-            GameObject g = Instantiate(Monster, _randomPosition, Quaternion.identity);
-            g.transform.LookAt(new Vector3(0f, 0f, 0f));
-            unityGameObjects.Add(g);
-        }
 
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        SetRanges();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    void Update()
+    {
+        if (countDown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+        }
+        countDown -= Time.deltaTime;
+
+        countDown = Mathf.Clamp(countDown, 0f, Mathf.Infinity);
+        waveCountdownText.text = string.Format("{0:00.00}", countDown);
+        enemyCountText.text = monsterCount.ToString();
+    }
+
+    private void SpawnEnemy()
+    {
+        _xAxis = UnityEngine.Random.Range(Min.x, Max.x) * (Random.Range(0, 2) * 2 - 1);
+        _yAxis = 5f;
+        _zAxis = UnityEngine.Random.Range(Min.z, Max.z) * (Random.Range(0, 2) * 2 - 1);
+        _randomPosition = new Vector3(_xAxis, _yAxis, _zAxis);
+        monsterCount++;
+        GameObject g = Instantiate(Monster, _randomPosition, Quaternion.identity);
+        g.transform.LookAt(new Vector3(0f, 0f, 0f));
     }
 
     public void DestroyMonster(GameObject monsterToDestroy)
     {
-        for (int i = 0; i < unityGameObjects.Count; i++)
-        {
-            // Check if list at index of i is equal to destroyedObject / list is containing destroyedObject
-            if (unityGameObjects[i] == monsterToDestroy)
-            {
-                audioSource.Play();
-                unityGameObjects.Remove(unityGameObjects[i]);
-                Destroy(monsterToDestroy);
-                return;
-            }
-        }
-
-
+        monsterCount--;
+        Destroy(monsterToDestroy);
+        return;
 
     }
+
+
+
 }
+
